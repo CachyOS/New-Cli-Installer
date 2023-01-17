3351a54332f51727e1e491d031c2c2e63bb0c945
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-01-18 03:10:15 +0400

    🧹 cleanup follow_process_log.hpp

--- src/follow_process_log.hpp
+++ src/follow_process_log.hpp
@@ -6,10 +6,8 @@
 
 #include <ftxui/dom/elements.hpp>  // for size, GREATER_THAN
 
-namespace tui {
-namespace detail {
-    void follow_process_log_widget(const std::vector<std::string>& vec, ftxui::Decorator box_size = size(ftxui::HEIGHT, ftxui::GREATER_THAN, 5)) noexcept;
-}  // namespace detail
-}  // namespace tui
+namespace tui::detail {
+void follow_process_log_widget(const std::vector<std::string>& vec, ftxui::Decorator box_size = size(ftxui::HEIGHT, ftxui::GREATER_THAN, 5)) noexcept;
+}  // namespace tui::detail
 
 #endif  // FOLLOW_PROCESS_LOG_HPP
