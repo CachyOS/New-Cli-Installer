45ade9ed50d5efd54c652d12ab5fa83899e526da
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2024-07-28 19:52:06 +0400

    🚧 gucc: add helper function to get UUID of device/part

--- gucc/include/gucc/fs_utils.hpp
+++ gucc/include/gucc/fs_utils.hpp
@@ -12,6 +12,9 @@ auto get_mountpoint_fs(std::string_view mountpoint) noexcept -> std::string;
 // Get SOURCE of mountpoint
 auto get_mountpoint_source(std::string_view mountpoint) noexcept -> std::string;
 
+// Get UUID of device/partition
+auto get_device_uuid(std::string_view device) noexcept -> std::string;
+
 }  // namespace gucc::fs::utils
 
 #endif  // FS_UTILS_HPP
--- gucc/src/fs_utils.cpp
+++ gucc/src/fs_utils.cpp
@@ -16,4 +16,8 @@ auto get_mountpoint_source(std::string_view mountpoint) noexcept -> std::string 
     return gucc::utils::exec(fmt::format(FMT_COMPILE("findmnt -ln -o SOURCE \"{}\""), mountpoint));
 }
 
+auto get_device_uuid(std::string_view device) noexcept -> std::string {
+    return gucc::utils::exec(fmt::format(FMT_COMPILE("lsblk -o UUID '{}' | awk 'NR==2'"), device));
+}
+
 }  // namespace gucc::fs::utils
