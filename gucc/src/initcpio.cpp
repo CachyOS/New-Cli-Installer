701e6107525e6e3e164b40c97ba8c0ffba12f05e
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2024-08-09 15:14:06 +0400

    👷 gucc: refactor initcpio

--- gucc/src/initcpio.cpp
+++ gucc/src/initcpio.cpp
@@ -10,6 +10,32 @@
 
 using namespace std::string_view_literals;
 
+namespace {
+
+constexpr auto modify_initcpio_line(std::string_view line, std::string_view modules, std::string_view files, std::string_view hooks) noexcept -> std::string {
+    if (line.starts_with("MODULES"sv)) {
+        return fmt::format(FMT_COMPILE("MODULES=({})"), modules);
+    } else if (line.starts_with("FILES"sv)) {
+        return fmt::format(FMT_COMPILE("FILES=({})"), files);
+    } else if (line.starts_with("HOOKS"sv)) {
+        return fmt::format(FMT_COMPILE("HOOKS=({})"), hooks);
+    }
+    return std::string{line.data(), line.size()};
+}
+
+constexpr auto modify_initcpio_fields(std::string_view file_content, std::string_view modules, std::string_view files, std::string_view hooks) noexcept -> std::string {
+    return file_content | std::ranges::views::split('\n')
+        | std::ranges::views::transform([&](auto&& rng) {
+              /* clang-format off */
+              auto&& line = std::string_view(&*rng.begin(), static_cast<size_t>(std::ranges::distance(rng)));
+              return modify_initcpio_line(line, modules, files, hooks);
+          })
+        | std::ranges::views::join_with('\n')
+        | std::ranges::to<std::string>();
+}
+
+}  // namespace
+
 namespace gucc::detail {
 
 bool Initcpio::write() const noexcept {
@@ -18,36 +44,12 @@ bool Initcpio::write() const noexcept {
         spdlog::error("[INITCPIO] '{}' error occurred!", m_file_path);
         return false;
     }
-    std::string&& result = file_content | std::ranges::views::split('\n')
-        | std::ranges::views::transform([&](auto&& rng) {
-              /* clang-format off */
-              auto&& line = std::string_view(&*rng.begin(), static_cast<size_t>(std::ranges::distance(rng)));
-              if (line.starts_with("MODULES"sv)) {
-                  auto&& formatted_modules = modules | std::ranges::views::join_with(' ')
-                                                     | std::ranges::to<std::string>();
-                  return fmt::format(FMT_COMPILE("MODULES=({})"), std::move(formatted_modules));
-              } else if (line.starts_with("FILES"sv)) {
-                  auto&& formatted_files = files | std::ranges::views::join_with(' ')
-                                                 | std::ranges::to<std::string>();
-                  return fmt::format(FMT_COMPILE("FILES=({})"), std::move(formatted_files));
-              } else if (line.starts_with("HOOKS"sv)) {
-                  auto&& formatted_hooks = hooks | std::ranges::views::join_with(' ')
-                                                 | std::ranges::to<std::string>();
-                  return fmt::format(FMT_COMPILE("HOOKS=({})"), std::move(formatted_hooks));
-              }
-              /* clang-format on */
-              return std::string{line.data(), line.size()};
-          })
-        | std::ranges::views::join_with('\n')
-        | std::ranges::to<std::string>();
+    const auto& formatted_modules = utils::join(modules, ' ');
+    const auto& formatted_files = utils::join(files, ' ');
+    const auto& formatted_hooks = utils::join(hooks, ' ');
 
-    std::ofstream mhinitcpio_file{m_file_path.data()};
-    if (!mhinitcpio_file.is_open()) {
-        spdlog::error("[INITCPIO] '{}' open failed: {}", m_file_path, std::strerror(errno));
-        return false;
-    }
-    mhinitcpio_file << result;
-    return true;
+    auto result = modify_initcpio_fields(file_content, formatted_modules, formatted_files, formatted_hooks);
+    return file_utils::create_file_for_overwrite(m_file_path, result);
 }
 
 bool Initcpio::parse_file() noexcept {
