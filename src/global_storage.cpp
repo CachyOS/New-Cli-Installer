7ad9fd5d8ec635e878b0f660b2f8d0678dc765e5
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2026-03-08 15:27:35 +0400

    👷 tui: display mode selection

--- src/global_storage.cpp
+++ src/global_storage.cpp
@@ -17,7 +17,7 @@ bool Config::initialize() noexcept {
     if (s_config) {
         s_config->m_data["cachepath"]    = "/var/cache/pacman/pkg/";
         s_config->m_data["hostcache"]    = static_cast<std::int32_t>(!fs::exists("/run/miso/bootmnt"));
-        s_config->m_data["menus"]        = 2;
+        s_config->m_data["menus"]        = -1;
         s_config->m_data["POST_INSTALL"] = "";
 
         s_config->m_data["H_INIT"] = "openrc";
