ad27a4fbf26f2b1cacfef6bb87d56c3514e6689c
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2026-07-19 16:05:45 +0400

    🧹 gucc: refactor sfdisk commands gen

--- gucc/src/partitioning.cpp
+++ gucc/src/partitioning.cpp
@@ -33,6 +33,42 @@ constexpr auto get_part_type_alias(std::string_view fsname) noexcept -> std::str
     return "L"sv;
 }
 
+// single sfdisk command line
+constexpr auto to_sfdisk_line(const gucc::fs::Partition& part) noexcept -> std::string {
+    const auto& fsname   = convert_fsname(part.fstype);
+    const auto& fs_alias = get_part_type_alias(fsname);
+
+    // L - alias 'linux'. Linux
+    // U - alias 'uefi'. EFI System partition
+    auto line = fmt::format(FMT_COMPILE("type={}"), fs_alias);
+
+    // The field size= support '+' and '-' in the same way as Unnamed-fields
+    // format. The default value of size indicates "as much as possible";
+    // i.e., until the next partition or end-of-device. A numerical argument is
+    // by default interpreted as a number of sectors, however if the size
+    // is followed by one of the multiplicative suffixes (KiB, MiB, GiB,
+    // TiB, PiB, EiB, ZiB and YiB) then the number is interpreted
+    // as the size of the partition in bytes and it is then aligned
+    // according to the device I/O limits. A '+' can be used instead of a
+    // number to enlarge the partition as much as possible. Note '+' is
+    // equivalent to the default behaviour for a new partition; existing
+    // partitions will be resized as required.
+    if (!part.size.empty()) {
+        line += fmt::format(FMT_COMPILE(",size={}"), part.size);
+    }
+
+    // set boot flag
+    if (fsname == "vfat"sv) {
+        // bootable is specified as [*|-], with as default not-bootable.
+        // The value of this field is irrelevant for Linux
+        // - when Linux runs it has been booted already
+        // - but it might play a role for certain boot loaders and for other operating systems.
+        line += ",bootable"s;
+    }
+    line += "\n"s;
+    return line;
+}
+
 }  // namespace
 
 namespace gucc::disk {
@@ -60,37 +96,7 @@ auto gen_sfdisk_command(const std::vector<fs::Partition>& partitions, bool is_ef
         &fs::Partition::device);
 
     for (const auto& part : partitions_filtered) {
-        const auto& fsname   = convert_fsname(part.fstype);
-        const auto& fs_alias = get_part_type_alias(fsname);
-
-        // L - alias 'linux'. Linux
-        // U - alias 'uefi'. EFI System partition
-        sfdisk_commands += fmt::format(FMT_COMPILE("type={}"), fs_alias);
-
-        // The field size= support '+' and '-' in the same way as Unnamed-fields
-        // format. The default value of size indicates "as much as possible";
-        // i.e., until the next partition or end-of-device. A numerical argument is
-        // by default interpreted as a number of sectors, however if the size
-        // is followed by one of the multiplicative suffixes (KiB, MiB, GiB,
-        // TiB, PiB, EiB, ZiB and YiB) then the number is interpreted
-        // as the size of the partition in bytes and it is then aligned
-        // according to the device I/O limits. A '+' can be used instead of a
-        // number to enlarge the partition as much as possible. Note '+' is
-        // equivalent to the default behaviour for a new partition; existing
-        // partitions will be resized as required.
-        if (!part.size.empty()) {
-            sfdisk_commands += fmt::format(FMT_COMPILE(",size={}"), part.size);
-        }
-
-        // set boot flag
-        if (fsname == "vfat"sv) {
-            // bootable is specified as [*|-], with as default not-bootable.
-            // The value of this field is irrelevant for Linux
-            // - when Linux runs it has been booted already
-            // - but it might play a role for certain boot loaders and for other operating systems.
-            sfdisk_commands += ",bootable"s;
-        }
-        sfdisk_commands += "\n"s;
+        sfdisk_commands += to_sfdisk_line(part);
     }
     return sfdisk_commands;
 }
